99437f4eb9dfcebf70039928b253f84e6a35749c
Jordan Partridge <jordan@partridge.rocks>
2025-07-28 22:10:34 -0700

    🔧 Bump version to 2.12.1

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'Conduit',
-    'version' => '2.12.0',
+    'version' => '2.12.1',
     'env' => 'development',
     'providers' => [
         0 => 'App\\Providers\\AppServiceProvider',
